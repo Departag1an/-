13d361889f74df9333354eec2d1006f6800355c1
AliceAuto <sgq2742798794@gmail.com>
2024-10-27 14:25:38 +0800

    d

---
+++ 算法/板子.cpp
@@ -0,0 +1,49 @@
+#include <cstdio>
+
+#define Dtype long long 
+
+using namespace std;
+//FAST input
+void in(Dtype * x) {
+	* x = 0;
+	char chace = getchar();
+	int f = 1;
+	while (chace - '0'<0 ||chace -'9'>0){
+		if (chace == EOF || chace == '\n')
+		{
+			return;
+		}
+		if (chace == '-') {
+			f = -1;
+			chace = getchar();
+			break;
+		}
+		chace = getchar();
+	}
+	if (chace - '0' < 0 || chace - '9' > 0)return;
+	while (chace - '0' >= -0 && chace - '9' <= 0) {
+		*x *= 10;
+		*x += chace - '0';
+		chace = getchar();
+	}
+	*x *= f;
+	return;
+
+}
+// FAST output
+void out(Dtype x) {
+	if (x < 0) { putchar('-'),x =-x; }
+	if (x < 10) { putchar(x + '0'); }
+	else { out(x / 10); putchar(x % 10 + '0'); }
+}
+
+
+
+int main() {
+	Dtype x;
+	in(&x);
+	out(x);
+
+
+	return 0;
+}
