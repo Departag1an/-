9642a61b868afbaf0242d1852260830e2372654f
AliceAuto <sgq2742798794@gmail.com>
2024-12-19 20:02:27 +0800

    d

---
+++ 算法/板子/快输快读.cpp
@@ -0,0 +1,62 @@
+int I32(){
+	char chace = getchar();
+	int f= 1;
+	int res = 0;
+	while (chace -'0'<0||chace - '9'>0){
+		if(chace==EOF)return res;
+		if(chace == '-')f = -f;
+		chace = getchar();
+	}
+	while(chace -'0'>=0&&chace -'9'<=0)	{
+		res= res *10 +chace - '0';
+		chace = getchar();
+	}
+	res *=f;
+	return res;
+}
+long long I64(){
+	char chace = getchar();
+	int f= 1;
+	long long  res = 0;
+	while (chace -'0'<0||chace - '9'>0){
+		if(chace==EOF)return res;
+		if(chace == '-')f = -f;
+		chace = getchar();
+	}
+	while(chace -'0'>=0&&chace -'9'<=0)	{
+		res= res *10 +chace - '0';
+		chace = getchar();
+	}
+	res *=f;
+	return res;
+}
+
+size_t I64t(){
+	char chace = getchar();
+	size_t res = 0;
+	while (chace -'0'<0||chace - '9'>0){
+		if(chace==EOF)return res;
+		chace = getchar();
+	}
+	while(chace -'0'>=0&&chace -'9'<=0)	{
+		res= res *10 +chace - '0';
+		chace = getchar();
+	}
+	return res;
+}
+void O64t(size_t x){
+	if(x<10)putchar(x+'0');
+	else O64t(x/10),putchar(x%10+'0');
+}
+
+void O64(long long x){
+	if(x<0)x= -x,putchar('-');
+	if(x<10)putchar(x+'0');
+	else O64t(x/10),putchar(x%10+'0');
+}
+void O32(int x){
+	if(x<0)x= -x,putchar('-');
+	if(x<10)putchar(x+'0');
+	else O64t(x/10),putchar(x%10+'0');
+}
+
---
+++ 算法/板子/预处理头.cpp
@@ -0,0 +1,12 @@
+#ifndef Preproccess
+#define Preproccess
+#include<bits/stdc++.h>
+using namespace std;
+#define REP(a,b,c,d) for (a = b;a!=c;a+=d)
+#define FastIOS {ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);}
+#define i64 long long
+#define i32 int
+#define s64 size_t
+#define f32 float
+#define f64 double
+#endif
